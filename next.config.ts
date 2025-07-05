9e1b9f6ff866c980898dc48f730f6af8852e65b6
schelskedevco <joe@schelske.dev>
2025-07-05 13:37:12 -0500

    remote patterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com", "tailwindcss.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "tailwindcss.com",
+      },
+    ],
   },
 };
 
