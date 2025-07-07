ad044f7e9b16b11eff00405f91595dd325c298e8
schelskedevco <joe@schelske.dev>
2025-07-07 18:01:09 -0500

    initial FAQ section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "./components/hero-section";
 import Footer from "./components/footer";
 import CTASection from "./components/cta-section";
+import FAQSection from "./components/faq-section";
 
 export default function LandingPage() {
   return (
     <>
       <HeroSection />
+      <FAQSection />
       <CTASection />
       <Footer />
     </>
