2c796af4c4138427803e5df98ba9333d84c15c73
schelskedevco <joe@schelske.dev>
2025-07-07 22:43:35 -0500

    initial content for link sharing

--- src/app/components/link-sharing-feature.tsx
+++ src/app/components/link-sharing-feature.tsx
@@ -5,10 +5,10 @@ export default function LinkSharingFeature() {
     <div className="bg-gray-50 py-24 sm:py-32">
       <div className="mx-auto max-w-2xl px-6 lg:max-w-7xl lg:px-8">
         <h2 className="text-center text-base/7 font-semibold text-indigo-600">
-          Deploy faster
+          Share your plan
         </h2>
         <p className="mx-auto mt-2 max-w-lg text-center text-4xl font-semibold tracking-tight text-balance text-gray-950 sm:text-5xl">
-          Everything you need to deploy your app
+          From family advice to Reddit feedback
         </p>
         <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-2">
           <div className="relative lg:row-span-2">
