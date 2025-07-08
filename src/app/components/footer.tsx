2b3088f87af851a94510be26e2adc740bb4adaa1
schelskedevco <joe@schelske.dev>
2025-07-08 00:59:27 -0500

    border on footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -74,7 +74,7 @@ const navigation = {
 
 export default function Footer() {
   return (
-    <footer className="bg-white">
+    <footer className="border-t border-gray-200 bg-white">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
         <nav
           aria-label="Footer"
