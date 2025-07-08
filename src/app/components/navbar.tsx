2bba72a9440aa49452287d4126070c9b8c710754
schelskedevco <joe@schelske.dev>
2025-07-07 23:26:56 -0500

    fix links

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { useState } from "react";
+import Link from "next/link";
 import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { FireIcon } from "@heroicons/react/24/solid";
 
 const navigation = [
-  { name: "How It Works", href: "#how-it-works" },
-  { name: "FAQ", href: "#faq" },
+  { name: "How It Works", href: "/#how-it-works" },
+  { name: "FAQ", href: "/#faq" },
   { name: "Pricing", href: "/pricing" },
   { name: "Get Started", href: "/dashboard/quick-plan" },
 ];
@@ -21,7 +22,7 @@ export default function Navbar() {
         className="flex items-center justify-between p-6 lg:px-8"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <Link href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Ignidash</span>
             <div className="flex items-center gap-2">
               <FireIcon className="h-8 w-8 text-rose-500" />
@@ -29,7 +30,7 @@ export default function Navbar() {
                 Ignidash
               </span>
             </div>
-          </a>
+          </Link>
         </div>
         <div className="flex lg:hidden">
           <button
@@ -43,13 +44,13 @@ export default function Navbar() {
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
           {navigation.map((item) => (
-            <a
+            <Link
               key={item.name}
               href={item.href}
               className="text-sm/6 font-semibold text-gray-900"
             >
               {item.name}
-            </a>
+            </Link>
           ))}
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
@@ -66,7 +67,7 @@ export default function Navbar() {
         <div className="fixed inset-0 z-50" />
         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Ignidash</span>
               <div className="flex items-center gap-2">
                 <FireIcon className="h-8 w-8 text-rose-500" />
@@ -74,7 +75,7 @@ export default function Navbar() {
                   Ignidash
                 </span>
               </div>
-            </a>
+            </Link>
             <button
               type="button"
               onClick={() => setMobileMenuOpen(false)}
@@ -88,13 +89,13 @@ export default function Navbar() {
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 {navigation.map((item) => (
-                  <a
+                  <Link
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
-                  </a>
+                  </Link>
                 ))}
               </div>
               <div className="py-6">
