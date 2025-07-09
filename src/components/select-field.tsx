7197b445c983730ea70d84eb78225181ed9fb736
schelskedevco <joe@schelske.dev>
2025-07-09 16:54:03 -0500

    select field update

--- src/components/select-field.tsx
+++ src/components/select-field.tsx
@@ -33,7 +33,7 @@ export function SelectField({
           name={id}
           value={value}
           onChange={onChange}
-          className="focus:outline-foreground col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
+          className="bg-background text-foreground outline-foreground/10 focus:outline-foreground col-start-1 row-start-1 w-full appearance-none rounded-md py-1.5 pr-8 pl-3 text-base outline-1 -outline-offset-1 focus:outline-2 focus:-outline-offset-2 sm:text-sm/6"
         >
           {options.map((option) => (
             <option key={option.value} value={option.value}>
