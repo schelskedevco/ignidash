d2e6ffde6903bf9d044e29f3e8f1819ad1a2693b
schelskedevco <joe@schelske.dev>
2025-07-09 16:55:05 -0500

    corner radius update

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -9,7 +9,7 @@ export function Card({ children, className }: CardProps) {
   return (
     <div
       className={cn(
-        "bg-emphasized-background my-4 overflow-hidden shadow-sm sm:rounded-lg",
+        "bg-emphasized-background my-4 overflow-hidden rounded-lg shadow-sm",
         className ?? ""
       )}
     >
