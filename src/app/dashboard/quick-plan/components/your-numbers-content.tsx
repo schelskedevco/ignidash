126556ab7d5c08593d8b05d40c31d200af4b81d6
schelskedevco <joe@schelske.dev>
2025-07-09 12:40:39 -0500

    right chevron button

--- src/app/dashboard/quick-plan/components/your-numbers-content.tsx
+++ src/app/dashboard/quick-plan/components/your-numbers-content.tsx
@@ -1,6 +1,7 @@
 import { Card } from "@/components/card";
 import { CoreInputs } from "./core-inputs";
 import { CoastFIRE, BaristaFIRE } from "./alternative-paths";
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
 
 export function YourNumbersContent() {
   return (
@@ -17,6 +18,14 @@ export function YourNumbersContent() {
         <Card>
           <CoreInputs />
         </Card>
+
+        <button
+          type="button"
+          className="bg-emphasized-background text-foreground hover:bg-background hover:ring-foreground/10 flex w-full items-center justify-between rounded-lg px-4 py-2.5 text-sm shadow-sm hover:ring-1 hover:ring-inset sm:px-6"
+        >
+          <span>Advanced</span>
+          <ChevronRightIcon className="h-5 w-5" />
+        </button>
       </div>
       <div className="border-foreground/10 mb-5 border-b pb-5">
         <div className="ml-2">
