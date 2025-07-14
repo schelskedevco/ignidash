81b6594e8d7ce005e0d6ed0d8e4e7d4a1ec318fd
schelskedevco <joe@schelske.dev>
2025-07-14 01:29:13 -0500

    emphasized backdrop

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -26,7 +26,7 @@ export default function Drawer({
     <Dialog open={open} onClose={setOpen} className="relative z-60">
       <DialogBackdrop
         transition
-        className="fixed inset-0 bg-zinc-900/80 transition-opacity duration-500 ease-in-out data-closed:opacity-0"
+        className="bg-emphasized-background fixed inset-0 transition-opacity duration-500 ease-in-out data-closed:opacity-0 sm:bg-zinc-900/80"
       />
 
       <div className="fixed inset-0 overflow-hidden">
