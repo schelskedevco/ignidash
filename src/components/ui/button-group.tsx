5d63b0891804f9fb3cee2df7e4bbd22b0e01b348
schelskedevco <joe@schelske.dev>
2025-07-19 21:39:20 -0500

    remove import

--- src/components/ui/button-group.tsx
+++ src/components/ui/button-group.tsx
@@ -3,19 +3,19 @@ export default function ButtonGroup() {
     <span className="isolate inline-flex w-full rounded-md shadow-xs">
       <button
         type="button"
-        className="bg-emphasized-background ring-border relative inline-flex items-center rounded-l-md px-3 py-2 text-sm font-semibold ring-1 ring-inset hover:bg-gray-50 focus:z-10"
+        className="bg-emphasized-background ring-border hover:bg-background relative inline-flex items-center rounded-l-md px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10"
       >
         Years
       </button>
       <button
         type="button"
-        className="bg-emphasized-background ring-border relative -ml-px inline-flex items-center px-3 py-2 text-sm font-semibold ring-1 ring-inset hover:bg-gray-50 focus:z-10"
+        className="bg-emphasized-background ring-border hover:bg-background relative -ml-px inline-flex items-center px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10"
       >
         Months
       </button>
       <button
         type="button"
-        className="bg-emphasized-background ring-border relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold ring-1 ring-inset hover:bg-gray-50 focus:z-10"
+        className="bg-emphasized-background ring-border hover:bg-background relative -ml-px inline-flex items-center rounded-r-md px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10"
       >
         Days
       </button>
