a1945fdc1e5f4da51eef3000afcf0d85f34dfc9d
schelskedevco <joe@schelske.dev>
2025-08-03 11:10:11 -0500

    formatNumber

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,8 +9,8 @@ export function formatNumber(num: number, fractionDigits: number = 2) {
   const absNum = Math.abs(num);
   const sign = num < 0 ? '-' : '';
 
-  if (absNum >= 1000000000) return sign + (absNum / 1000000000).toFixed(fractionDigits) + 'B';
-  if (absNum >= 1000000) return sign + (absNum / 1000000).toFixed(fractionDigits) + 'M';
-  if (absNum >= 1000) return sign + (absNum / 1000).toFixed(fractionDigits) + 'k';
+  if (absNum >= 1000000000) return sign + (absNum / 1000000000).toFixed(2) + 'B';
+  if (absNum >= 1000000) return sign + (absNum / 1000000).toFixed(2) + 'M';
+  if (absNum >= 1000) return sign + (absNum / 1000).toFixed(1) + 'k';
   return sign + absNum.toFixed(fractionDigits);
 }
