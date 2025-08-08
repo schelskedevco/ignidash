b9a656de3dc80bd27327bca1319693795fc98465
schelskedevco <joe@schelske.dev>
2025-08-08 12:12:11 -0500

    hover ring for sidebar

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -34,5 +34,13 @@ export default function ModeToggle() {
     newTheme = 'dark';
   }
 
-  return <IconButton icon={icon} label={label} onClick={() => setTheme(newTheme)} surfaceColor="emphasized" />;
+  return (
+    <IconButton
+      icon={icon}
+      label={label}
+      onClick={() => setTheme(newTheme)}
+      surfaceColor="emphasized"
+      className="ring-border border-0 hover:ring"
+    />
+  );
 }
