36cf46baaf5b82f0e5f39520dc8a20453088aeb8
schelskedevco <joe@schelske.dev>
2025-08-15 22:15:07 -0500

    container query for grid

--- src/components/layout/main-area.tsx
+++ src/components/layout/main-area.tsx
@@ -5,7 +5,7 @@ interface MainAreaProps {
 export default function MainArea({ children }: MainAreaProps) {
   return (
     <main className="lg:pl-72">
-      <div className="xl:pl-96">
+      <div className="@container xl:pl-96">
         <div className="px-4 py-2 sm:px-6 lg:px-8 xl:py-6">{children}</div>
       </div>
     </main>
