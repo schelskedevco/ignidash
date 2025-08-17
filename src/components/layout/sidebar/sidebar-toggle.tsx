8094d2a967052ea291b8583dc382931d8492c8c2
schelskedevco <joe@schelske.dev>
2025-08-17 15:32:14 -0500

    update sidebar toggle

--- src/components/layout/sidebar/sidebar-toggle.tsx
+++ src/components/layout/sidebar/sidebar-toggle.tsx
@@ -3,13 +3,5 @@ import { PanelLeft } from 'lucide-react';
 import IconButton from '@/components/ui/icon-button';
 
 export default function SidebarToggle() {
-  return (
-    <IconButton
-      icon={PanelLeft}
-      label="Toggle sidebar"
-      onClick={() => {}}
-      surfaceColor="emphasized"
-      className="ring-border border-0 hover:ring"
-    />
-  );
+  return <IconButton icon={PanelLeft} label="Toggle sidebar" onClick={() => {}} surfaceColor="emphasized" />;
 }
