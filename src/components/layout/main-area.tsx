ea201ee4231a1f290ed0db6004a284e2e6442972
schelskedevco <joe@schelske.dev>
2025-08-17 11:25:40 -0500

    fixed positioning for main area

--- src/components/layout/main-area.tsx
+++ src/components/layout/main-area.tsx
@@ -4,8 +4,8 @@ interface MainAreaProps {
 
 export default function MainArea({ children }: MainAreaProps) {
   return (
-    <main className="lg:pl-72">
-      <div className="@container xl:pl-96">
+    <main className="lg:pl-72 xl:fixed xl:inset-0 xl:left-96 xl:overflow-y-auto">
+      <div className="@container">
         <div className="px-4 sm:px-6 lg:px-8">{children}</div>
       </div>
     </main>
