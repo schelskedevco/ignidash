0162985dd1cde13908f79b57f82809dfb51299a1
schelskedevco <joe@schelske.dev>
2025-08-19 16:43:48 -0500

    stone instead zinc divider

--- src/components/catalyst/divider.tsx
+++ src/components/catalyst/divider.tsx
@@ -8,8 +8,8 @@ export function Divider({ soft = false, className, ...props }: { soft?: boolean 
       className={clsx(
         className,
         'w-full border-t',
-        soft && 'border-zinc-950/5 dark:border-white/5',
-        !soft && 'border-zinc-950/10 dark:border-white/10'
+        soft && 'border-stone-950/5 dark:border-white/5',
+        !soft && 'border-stone-950/10 dark:border-white/10'
       )}
     />
   );
