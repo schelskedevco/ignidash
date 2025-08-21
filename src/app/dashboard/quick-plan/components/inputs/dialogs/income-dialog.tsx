1bd6e0fceea33f5f4b98c89581ae631fba01eec7
schelskedevco <joe@schelske.dev>
2025-08-20 19:13:53 -0500

    income dialog

---
+++ src/app/dashboard/quick-plan/components/inputs/dialogs/income-dialog.tsx
@@ -0,0 +1,10 @@
+import { DialogTitle, DialogBody } from '@/components/catalyst/dialog';
+
+export default function IncomeDialog() {
+  return (
+    <>
+      <DialogTitle>Income</DialogTitle>
+      <DialogBody className="space-y-4"></DialogBody>
+    </>
+  );
+}
--- src/app/dashboard/quick-plan/components/inputs/sections/income-section.tsx
+++ src/app/dashboard/quick-plan/components/inputs/sections/income-section.tsx
@@ -1,17 +1,32 @@
+'use client';
+
+import { useState } from 'react';
 import { BanknoteArrowUpIcon } from 'lucide-react';
 
 import DisclosureSection from '@/components/ui/disclosure-section';
+import { Dialog } from '@/components/catalyst/dialog';
+
+import IncomeDialog from '../dialogs/income-dialog';
 
 export default function IncomeSection() {
+  const [incomeDialogOpen, setIncomeDialogOpen] = useState(false);
+
   return (
-    <DisclosureSection title="Income" icon={BanknoteArrowUpIcon}>
-      <button
-        type="button"
-        className="focus-outline relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 dark:border-white/15 dark:hover:border-white/25"
-      >
-        <BanknoteArrowUpIcon aria-hidden="true" className="text-primary mx-auto size-12" />
-        <span className="mt-2 block text-sm font-semibold text-gray-900 dark:text-white">Create an income</span>
-      </button>
-    </DisclosureSection>
+    <>
+      <DisclosureSection title="Income" icon={BanknoteArrowUpIcon}>
+        <button
+          type="button"
+          className="focus-outline relative block w-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 dark:border-white/15 dark:hover:border-white/25"
+          onClick={() => setIncomeDialogOpen(true)}
+        >
+          <BanknoteArrowUpIcon aria-hidden="true" className="text-primary mx-auto size-12" />
+          <span className="mt-2 block text-sm font-semibold text-gray-900 dark:text-white">Create an income</span>
+        </button>
+      </DisclosureSection>
+
+      <Dialog open={incomeDialogOpen} onClose={setIncomeDialogOpen}>
+        <IncomeDialog />
+      </Dialog>
+    </>
   );
 }
