8b2801f1d14fd57bc2dd0182051149add8923212
schelskedevco <joe@schelske.dev>
2025-08-26 02:36:32 -0500

    tabindex -1 for main

--- src/components/layout/main-area.tsx
+++ src/components/layout/main-area.tsx
@@ -4,7 +4,10 @@ interface MainAreaProps {
 
 export default function MainArea({ children }: MainAreaProps) {
   return (
-    <main className="h-full lg:pl-72 group-data-[state=collapsed]/sidebar:lg:pl-16 xl:fixed xl:inset-0 xl:left-96 xl:overflow-y-auto group-data-[state=collapsed]/sidebar:xl:left-152">
+    <main
+      tabIndex={-1}
+      className="h-full lg:pl-72 group-data-[state=collapsed]/sidebar:lg:pl-16 xl:fixed xl:inset-0 xl:left-96 xl:overflow-y-auto group-data-[state=collapsed]/sidebar:xl:left-152"
+    >
       <div className="@container h-full">
         <div className="h-full px-2 sm:px-3 lg:px-4">{children}</div>
       </div>
