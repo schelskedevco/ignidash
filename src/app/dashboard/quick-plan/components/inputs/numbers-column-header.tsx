74d7b053f4b5e8529004fe27de954db3d1eb9c38
schelskedevco <joe@schelske.dev>
2025-08-31 17:19:43 -0500

    new drawers

--- src/app/dashboard/quick-plan/components/inputs/numbers-column-header.tsx
+++ src/app/dashboard/quick-plan/components/inputs/numbers-column-header.tsx
@@ -1,16 +1,21 @@
 'use client';
 
 import { useState } from 'react';
-import { CalculatorIcon, AdjustmentsHorizontalIcon } from '@heroicons/react/24/outline';
+import { CalculatorIcon, AdjustmentsHorizontalIcon, ArrowTrendingUpIcon } from '@heroicons/react/24/outline';
 
 import IconButton from '@/components/ui/icon-button';
 import Drawer from '@/components/ui/drawer';
 import ColumnHeader from '@/components/ui/column-header';
 
 import PreferencesDrawer from './drawers/preferences-drawer';
+import MarketAssumptionsDrawer from './drawers/market-assumptions-drawer';
+import SimulationPreferencesDrawer from './drawers/simulation-preferences-drawer';
 
 export default function NumbersColumnHeader() {
   const [preferencesOpen, setPreferencesOpen] = useState(false);
+
+  const [marketAssumptionsOpen, setMarketAssumptionsOpen] = useState(false);
+  const [simulationPreferencesOpen, setSimulationPreferencesOpen] = useState(false);
 
   const titleComponent = (
     <div className="flex items-center gap-2">
@@ -25,18 +30,34 @@ export default function NumbersColumnHeader() {
         title="Numbers"
         icon={CalculatorIcon}
         iconButton={
-          <IconButton
-            icon={AdjustmentsHorizontalIcon}
-            label="Preferences"
-            onClick={() => setPreferencesOpen(true)}
-            surfaceColor="emphasized"
-          />
+          <div className="flex items-center">
+            <IconButton
+              icon={ArrowTrendingUpIcon}
+              label="Market Assumptions"
+              onClick={() => setMarketAssumptionsOpen(true)}
+              surfaceColor="emphasized"
+            />
+            <IconButton
+              icon={AdjustmentsHorizontalIcon}
+              label="Preferences"
+              onClick={() => setPreferencesOpen(true)}
+              surfaceColor="emphasized"
+            />
+          </div>
         }
         className="left-76 w-96 border-r group-data-[state=collapsed]/sidebar:left-20"
       />
 
       <Drawer open={preferencesOpen} setOpen={setPreferencesOpen} title={titleComponent}>
         <PreferencesDrawer />
+      </Drawer>
+
+      <Drawer open={marketAssumptionsOpen} setOpen={setMarketAssumptionsOpen} title={titleComponent}>
+        <MarketAssumptionsDrawer />
+      </Drawer>
+
+      <Drawer open={simulationPreferencesOpen} setOpen={setSimulationPreferencesOpen} title={titleComponent}>
+        <SimulationPreferencesDrawer />
       </Drawer>
     </>
   );
