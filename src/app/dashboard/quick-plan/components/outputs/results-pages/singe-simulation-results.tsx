f700e22af3c8c55f635f9f5558b23e4cb60a7464
schelskedevco <joe@schelske.dev>
2025-09-07 19:01:44 -0500

    remove section header

--- src/app/dashboard/quick-plan/components/outputs/results-pages/singe-simulation-results.tsx
+++ src/app/dashboard/quick-plan/components/outputs/results-pages/singe-simulation-results.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useFixedReturnsSimulationV2 } from '@/lib/stores/quick-plan-store';
-import SectionHeader from '@/components/ui/section-header';
 import SectionContainer from '@/components/ui/section-container';
 
 import SingleSimulationMetrics from '../single-simulation-metrics';
@@ -12,7 +11,6 @@ export default function SingleSimulationResults() {
 
   return (
     <SectionContainer showBottomBorder>
-      <SectionHeader title="Key Metrics" desc="A snapshot of your simulation's most important results." />
       <SingleSimulationMetrics result={simulationResult} />
     </SectionContainer>
   );
