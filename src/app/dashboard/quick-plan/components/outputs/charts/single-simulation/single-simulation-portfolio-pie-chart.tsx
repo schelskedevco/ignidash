2367636087afb71d04bba0981c9674e40a2e56c2
schelskedevco <joe@schelske.dev>
2025-09-08 23:57:00 -0500

    padding

--- src/app/dashboard/quick-plan/components/outputs/charts/single-simulation/single-simulation-portfolio-pie-chart.tsx
+++ src/app/dashboard/quick-plan/components/outputs/charts/single-simulation/single-simulation-portfolio-pie-chart.tsx
@@ -114,6 +114,7 @@ export default function SingleSimulationPortfolioPieChart({ rawChartData, select
             innerRadius={60}
             outerRadius={80}
             fill="#8884d8"
+            paddingAngle={10}
             dataKey="value"
           >
             {chartData.map((entry, index) => (
