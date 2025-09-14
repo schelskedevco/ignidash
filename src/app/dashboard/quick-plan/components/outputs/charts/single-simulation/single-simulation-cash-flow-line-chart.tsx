8e1fe6cb996c40f989017e2ba652d08e50601688
schelskedevco <joe@schelske.dev>
2025-09-14 18:05:54 -0500

    colors tooltip

--- src/app/dashboard/quick-plan/components/outputs/charts/single-simulation/single-simulation-cash-flow-line-chart.tsx
+++ src/app/dashboard/quick-plan/components/outputs/charts/single-simulation/single-simulation-cash-flow-line-chart.tsx
@@ -46,7 +46,7 @@ const CustomTooltip = ({ active, payload, label, startAge, disabled }: CustomToo
             className="border-foreground/50 flex justify-between rounded-lg border px-2 text-sm"
           >
             <span className="mr-2">{`${formatChartString(entry.dataKey)}:`}</span>
-            <span className="ml-1 font-semibold">{formatNumber(entry.value, 3, '$')}</span>
+            <span className="ml-1 font-semibold">{formatNumber(entry.value, 1, '$')}</span>
           </p>
         ))}
       </div>
@@ -146,9 +146,9 @@ export default function SingleSimulationCashFlowLineChart({
       break;
   }
 
-  const gridColor = resolvedTheme === 'dark' ? '#374151' : '#d1d5db'; // gray-700 : gray-300
-  const foregroundColor = resolvedTheme === 'dark' ? '#f3f4f6' : '#111827'; // gray-100 : gray-900
-  const foregroundMutedColor = resolvedTheme === 'dark' ? '#d1d5db' : '#4b5563'; // gray-300 : gray-600
+  const gridColor = resolvedTheme === 'dark' ? '#44403c' : '#d6d3d1'; // stone-700 : stone-300
+  const foregroundColor = resolvedTheme === 'dark' ? '#f5f5f4' : '#1c1917'; // stone-100 : stone-900
+  const foregroundMutedColor = resolvedTheme === 'dark' ? '#d6d3d1' : '#57534e'; // stone-300 : stone-600
 
   const calculateInterval = useCallback((dataLength: number, desiredTicks = 8) => {
     if (dataLength <= desiredTicks) return 0;
