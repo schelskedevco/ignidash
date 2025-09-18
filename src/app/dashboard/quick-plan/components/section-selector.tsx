47cc04efd20d4ffeb32f8b57b260e01d4e66a810
schelskedevco <joe@schelske.dev>
2025-09-18 14:07:17 -0500

    lowercase

--- src/app/dashboard/quick-plan/components/section-selector.tsx
+++ src/app/dashboard/quick-plan/components/section-selector.tsx
@@ -77,7 +77,7 @@ export default function SectionSelector({ activeSection, setActiveSection }: Sec
                 onClick={() => setActiveSection(tab.value)}
                 aria-current={tab.value === activeSection ? 'page' : undefined}
                 className={cn(
-                  'text-muted-foreground focus-visible:ring-primary flex items-center gap-2 p-2 uppercase focus-visible:ring-2 focus-visible:outline-none focus-visible:ring-inset lg:p-4',
+                  'text-muted-foreground focus-visible:ring-primary flex items-center gap-2 p-2 lowercase focus-visible:ring-2 focus-visible:outline-none focus-visible:ring-inset lg:p-4',
                   { 'text-foreground': tab.value === activeSection },
                   { 'hover:bg-background': tab.value !== activeSection }
                 )}
