724816d92f63311e8dc4243d87bf138d2d8d89af
schelskedevco <joe@schelske.dev>
2025-09-23 19:26:06 -0500

    card

--- src/app/dashboard/quick-plan/components/outputs/sections/single-simulation-data-list-section.tsx
+++ src/app/dashboard/quick-plan/components/outputs/sections/single-simulation-data-list-section.tsx
@@ -14,7 +14,7 @@ interface DataListCardProps {
   dp: SimulationDataPoint;
 }
 
-function PortfolioDataListCard({ dp }: DataListCardProps) {
+function PortfolioAndReturnsDataListCard({ dp }: DataListCardProps) {
   const [checked, setChecked] = useState(true);
 
   const portfolioData = dp.portfolio;
@@ -51,42 +51,47 @@ function PortfolioDataListCard({ dp }: DataListCardProps) {
   }
 
   return (
-    <Card>
-      <div className="flex w-full items-center justify-between">
-        <Subheading level={4}>Portfolio</Subheading>
-        <Switch aria-label="Toggle portfolio data view mode" checked={checked} onChange={setChecked} />
-      </div>
-      <DescriptionList>
-        {checked ? (
-          <>
-            <DescriptionTerm>Stocks</DescriptionTerm>
-            <DescriptionDetails>{`${formatNumber(totalValue * stocksAllocation, 2, '$')} (${formatNumber(stocksAllocation * 100, 1)}%)`}</DescriptionDetails>
+    <div className="flex flex-col gap-2">
+      <Card className="mb-0">
+        <div className="flex w-full items-center justify-between">
+          <Subheading level={4}>Portfolio</Subheading>
+          <Switch aria-label="Toggle portfolio data view mode" checked={checked} onChange={setChecked} />
+        </div>
+        <DescriptionList>
+          {checked ? (
+            <>
+              <DescriptionTerm>Stocks</DescriptionTerm>
+              <DescriptionDetails>{`${formatNumber(totalValue * stocksAllocation, 2, '$')} (${formatNumber(stocksAllocation * 100, 1)}%)`}</DescriptionDetails>
 
-            <DescriptionTerm>Bonds</DescriptionTerm>
-            <DescriptionDetails>{`${formatNumber(totalValue * bondsAllocation, 2, '$')} (${formatNumber(bondsAllocation * 100, 1)}%)`}</DescriptionDetails>
+              <DescriptionTerm>Bonds</DescriptionTerm>
+              <DescriptionDetails>{`${formatNumber(totalValue * bondsAllocation, 2, '$')} (${formatNumber(bondsAllocation * 100, 1)}%)`}</DescriptionDetails>
 
-            <DescriptionTerm>Cash</DescriptionTerm>
-            <DescriptionDetails>{`${formatNumber(totalValue * cashAllocation, 2, '$')} (${formatNumber(cashAllocation * 100, 1)}%)`}</DescriptionDetails>
-          </>
-        ) : (
-          <>
-            <DescriptionTerm>Taxable</DescriptionTerm>
-            <DescriptionDetails>{`${formatNumber(taxable, 2, '$')} (${formatNumber((taxable / totalValue) * 100, 1)}%)`}</DescriptionDetails>
+              <DescriptionTerm>Cash</DescriptionTerm>
+              <DescriptionDetails>{`${formatNumber(totalValue * cashAllocation, 2, '$')} (${formatNumber(cashAllocation * 100, 1)}%)`}</DescriptionDetails>
+            </>
+          ) : (
+            <>
+              <DescriptionTerm>Taxable</DescriptionTerm>
+              <DescriptionDetails>{`${formatNumber(taxable, 2, '$')} (${formatNumber((taxable / totalValue) * 100, 1)}%)`}</DescriptionDetails>
 
-            <DescriptionTerm>Tax Deferred</DescriptionTerm>
-            <DescriptionDetails>{`${formatNumber(taxDeferred, 2, '$')} (${formatNumber((taxDeferred / totalValue) * 100, 1)}%)`}</DescriptionDetails>
+              <DescriptionTerm>Tax Deferred</DescriptionTerm>
+              <DescriptionDetails>{`${formatNumber(taxDeferred, 2, '$')} (${formatNumber((taxDeferred / totalValue) * 100, 1)}%)`}</DescriptionDetails>
 
-            <DescriptionTerm>Tax Free</DescriptionTerm>
-            <DescriptionDetails>{`${formatNumber(taxFree, 2, '$')} (${formatNumber((taxFree / totalValue) * 100, 1)}%)`}</DescriptionDetails>
+              <DescriptionTerm>Tax Free</DescriptionTerm>
+              <DescriptionDetails>{`${formatNumber(taxFree, 2, '$')} (${formatNumber((taxFree / totalValue) * 100, 1)}%)`}</DescriptionDetails>
 
-            <DescriptionTerm>Cash Savings</DescriptionTerm>
-            <DescriptionDetails>{`${formatNumber(cashSavings, 2, '$')} (${formatNumber((cashSavings / totalValue) * 100, 1)}%)`}</DescriptionDetails>
-          </>
-        )}
-        <DescriptionTerm className="font-semibold">Total Value</DescriptionTerm>
-        <DescriptionDetails className="font-semibold">{formatNumber(totalValue, 2, '$')}</DescriptionDetails>
-      </DescriptionList>
-    </Card>
+              <DescriptionTerm>Cash Savings</DescriptionTerm>
+              <DescriptionDetails>{`${formatNumber(cashSavings, 2, '$')} (${formatNumber((cashSavings / totalValue) * 100, 1)}%)`}</DescriptionDetails>
+            </>
+          )}
+          <DescriptionTerm className="font-semibold">Total Value</DescriptionTerm>
+          <DescriptionDetails className="font-semibold">{formatNumber(totalValue, 2, '$')}</DescriptionDetails>
+        </DescriptionList>
+      </Card>
+      <Card className="mt-0">
+        <Subheading level={4}>Returns</Subheading>
+      </Card>
+    </div>
   );
 }
 
@@ -188,7 +193,7 @@ export default function SingleSimulationDataListSection({ simulation, selectedAg
   return (
     <SectionContainer showBottomBorder>
       <div className="grid grid-cols-1 gap-2 @3xl:grid-cols-2 @5xl:grid-cols-3">
-        <PortfolioDataListCard dp={dp} />
+        <PortfolioAndReturnsDataListCard dp={dp} />
         <CashFlowDataListCard dp={dp} />
         <TaxesDataListCard dp={dp} />
       </div>
