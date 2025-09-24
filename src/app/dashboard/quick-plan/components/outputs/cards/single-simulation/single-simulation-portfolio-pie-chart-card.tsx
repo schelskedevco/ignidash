76502a84dd910e2b3d91d9036988cb1571ae030a
schelskedevco <joe@schelske.dev>
2025-09-24 12:38:48 -0500

    pie chart

--- src/app/dashboard/quick-plan/components/outputs/cards/single-simulation/single-simulation-portfolio-pie-chart-card.tsx
+++ src/app/dashboard/quick-plan/components/outputs/cards/single-simulation/single-simulation-portfolio-pie-chart-card.tsx
@@ -1,11 +1,7 @@
 'use client';
 
-import { Fragment } from 'react';
-
 import type { SingleSimulationPortfolioChartDataPoint } from '@/lib/types/chart-data-points';
 import Card from '@/components/ui/card';
-import { DescriptionDetails, DescriptionList, DescriptionTerm } from '@/components/catalyst/description-list';
-import { formatNumber, formatChartString } from '@/lib/utils';
 
 import SingleSimulationPortfolioPieChart from '../../charts/single-simulation/single-simulation-portfolio-pie-chart';
 
@@ -71,39 +67,15 @@ export default function SingleSimulationPortfolioAssetTypePieChartCard({
       break;
   }
 
-  const totalValue = chartData.reduce((acc, curr) => acc + curr.value, 0);
-
   return (
-    <div className="grid grid-cols-1 gap-2 @3xl:grid-cols-2">
-      <Card className="my-0">
-        <div className="mb-4 flex items-center justify-between">
-          <h4 className="text-foreground flex items-center text-lg font-semibold">
-            <span className="mr-2">{title}</span>
-            <span className="text-muted-foreground hidden sm:inline">Age {selectedAge}</span>
-          </h4>
-        </div>
-        <SingleSimulationPortfolioPieChart chartData={chartData} />
-      </Card>
-      <Card className="my-0 hidden @3xl:block">
-        <div className="mb-4 flex items-center justify-between">
-          <h4 className="text-foreground flex items-center text-lg font-semibold">
-            <span className="mr-2">Net Worth</span>
-            <span className="text-muted-foreground hidden sm:inline">Age {selectedAge}</span>
-          </h4>
-        </div>
-        <div className="flex h-64 w-full flex-col sm:h-72 lg:h-80">
-          <DescriptionList>
-            {chartData.map((entry, index) => (
-              <Fragment key={entry.name}>
-                <DescriptionTerm>{formatChartString(entry.name)}</DescriptionTerm>
-                <DescriptionDetails>{formatNumber(entry.value, 2, '$')}</DescriptionDetails>
-              </Fragment>
-            ))}
-            <DescriptionTerm className="font-bold">Total Value</DescriptionTerm>
-            <DescriptionDetails className="font-bold">{formatNumber(totalValue, 2, '$')}</DescriptionDetails>
-          </DescriptionList>
-        </div>
-      </Card>
-    </div>
+    <Card className="my-0">
+      <div className="mb-4 flex items-center justify-between">
+        <h4 className="text-foreground flex items-center text-lg font-semibold">
+          <span className="mr-2">{title}</span>
+          <span className="text-muted-foreground hidden sm:inline">Age {selectedAge}</span>
+        </h4>
+      </div>
+      <SingleSimulationPortfolioPieChart chartData={chartData} />
+    </Card>
   );
 }
