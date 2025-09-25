701822c217992f1ac91aa2e954c499ae400b4a34
schelskedevco <joe@schelske.dev>
2025-09-25 18:48:48 -0500

    update content

--- src/app/dashboard/quick-plan/components/outputs/sections/single-simulation-data-list-section.tsx
+++ src/app/dashboard/quick-plan/components/outputs/sections/single-simulation-data-list-section.tsx
@@ -147,7 +147,7 @@ function TaxesDataListCardV2({ dp, selectedAge }: DataListCardProps) {
         <span className="text-muted-foreground hidden sm:inline">Age {selectedAge}</span>
       </Subheading>
       <DescriptionList>
-        <DescriptionTerm>Total Gross Income</DescriptionTerm>
+        <DescriptionTerm>Gross Income</DescriptionTerm>
         <DescriptionDetails>{formatNumber(grossIncome, 2, '$')}</DescriptionDetails>
 
         <DescriptionTerm>Income Tax</DescriptionTerm>
