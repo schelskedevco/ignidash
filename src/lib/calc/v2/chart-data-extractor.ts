56c6b906d93746ec07fb7901c7300d8221c493b3
schelskedevco <joe@schelske.dev>
2025-09-26 22:18:24 -0500

    table data extractor

---
+++ src/lib/calc/v2/chart-data-extractor.ts
@@ -0,0 +1,271 @@
+import type {
+  SingleSimulationPortfolioChartDataPoint,
+  SingleSimulationCashFlowChartDataPoint,
+  SingleSimulationTaxesChartDataPoint,
+  SingleSimulationReturnsChartDataPoint,
+  SingleSimulationContributionsChartDataPoint,
+  SingleSimulationWithdrawalsChartDataPoint,
+} from '@/lib/types/chart-data-points';
+
+import type { SimulationResult } from './simulation-engine';
+
+export class ChartDataExtractor {
+  extractSingleSimulationPortfolioChartData(simulation: SimulationResult): SingleSimulationPortfolioChartDataPoint[] {
+    const startAge = simulation.context.startAge;
+    const startDateYear = new Date().getFullYear();
+
+    return simulation.data.map((data) => {
+      const currDateYear = new Date(data.date).getFullYear();
+
+      const portfolioData = data.portfolio;
+      const totalValue = portfolioData.totalValue;
+
+      const assetAllocation = portfolioData.assetAllocation ?? { stocks: 0, bonds: 0, cash: 0 };
+      const stocksAllocation = assetAllocation.stocks;
+      const bondsAllocation = assetAllocation.bonds;
+      const cashAllocation = assetAllocation.cash;
+
+      let cashSavings = 0;
+      let taxableBrokerage = 0;
+      let taxDeferred = 0;
+      let taxFree = 0;
+
+      for (const account of Object.values(portfolioData.perAccountData)) {
+        switch (account.type) {
+          case 'savings':
+            cashSavings += account.totalValue;
+            break;
+          case 'taxableBrokerage':
+            taxableBrokerage += account.totalValue;
+            break;
+          case '401k':
+          case 'ira':
+          case 'hsa':
+            taxDeferred += account.totalValue;
+            break;
+          case 'roth401k':
+          case 'rothIra':
+            taxFree += account.totalValue;
+            break;
+        }
+      }
+
+      return {
+        age: currDateYear - startDateYear + startAge,
+        stocks: totalValue * stocksAllocation,
+        bonds: totalValue * bondsAllocation,
+        cash: totalValue * cashAllocation,
+        taxableBrokerage,
+        taxDeferred,
+        taxFree,
+        cashSavings,
+        perAccountData: Object.values(portfolioData.perAccountData),
+      };
+    });
+  }
+
+  extractSingleSimulationCashFlowChartData(simulation: SimulationResult): SingleSimulationCashFlowChartDataPoint[] {
+    const startAge = simulation.context.startAge;
+    const startDateYear = new Date().getFullYear();
+
+    return simulation.data.slice(1).map((data) => {
+      const currDateYear = new Date(data.date).getFullYear();
+
+      const portfolioData = data.portfolio;
+
+      let taxDeferredWithdrawals = 0;
+      for (const account of Object.values(portfolioData.perAccountData)) {
+        switch (account.type) {
+          case '401k':
+          case 'ira':
+          case 'hsa':
+            taxDeferredWithdrawals += account.withdrawalsForPeriod;
+            break;
+          default:
+            break;
+        }
+      }
+
+      const incomesData = data.incomes!;
+      const expensesData = data.expenses!;
+      const taxesData = data.taxes!;
+
+      const ordinaryIncome = incomesData?.totalGrossIncome ?? 0;
+      const grossIncome = ordinaryIncome + taxDeferredWithdrawals;
+      const incomeTax = taxesData?.incomeTaxes.incomeTaxAmount ?? 0;
+      const expenses = expensesData?.totalExpenses ?? 0;
+      const netIncome = grossIncome - incomeTax;
+      const netCashFlow = netIncome - expenses;
+      const savingsRate = netIncome > 0 ? (netCashFlow / netIncome) * 100 : null;
+
+      return {
+        age: currDateYear - startDateYear + startAge,
+        perIncomeData: Object.values(incomesData.perIncomeData),
+        perExpenseData: Object.values(expensesData.perExpenseData),
+        ordinaryIncome,
+        taxDeferredWithdrawals,
+        grossIncome,
+        incomeTax,
+        expenses,
+        netIncome,
+        netCashFlow,
+        savingsRate,
+      };
+    });
+  }
+
+  extractSingleSimulationTaxesChartData(simulation: SimulationResult): SingleSimulationTaxesChartDataPoint[] {
+    const startAge = simulation.context.startAge;
+    const startDateYear = new Date().getFullYear();
+
+    return simulation.data.slice(1).map((data) => {
+      const currDateYear = new Date(data.date).getFullYear();
+
+      const taxesData = data.taxes!;
+
+      return {
+        age: currDateYear - startDateYear + startAge,
+        taxableOrdinaryIncome: taxesData.incomeTaxes.taxableOrdinaryIncome,
+        incomeTaxAmount: taxesData.incomeTaxes.incomeTaxAmount,
+        effectiveIncomeTaxRate: taxesData.incomeTaxes.effectiveIncomeTaxRate,
+        topMarginalIncomeTaxRate: taxesData.incomeTaxes.topMarginalTaxRate,
+        netIncome: taxesData.incomeTaxes.netIncome,
+        capitalLossDeduction: taxesData.incomeTaxes.capitalLossDeduction,
+        taxableCapGains: taxesData.capitalGainsTaxes.taxableCapitalGains,
+        capGainsTaxAmount: taxesData.capitalGainsTaxes.capitalGainsTaxAmount,
+        effectiveCapGainsTaxRate: taxesData.capitalGainsTaxes.effectiveCapitalGainsTaxRate,
+        topMarginalCapGainsTaxRate: taxesData.capitalGainsTaxes.topMarginalCapitalGainsTaxRate,
+        netCapGains: taxesData.capitalGainsTaxes.netCapitalGains,
+        totalTaxableIncome: taxesData.totalTaxableIncome,
+        totalTaxesAmount: taxesData.incomeTaxes.incomeTaxAmount + taxesData.capitalGainsTaxes.capitalGainsTaxAmount,
+        totalNetIncome: taxesData.incomeTaxes.netIncome + taxesData.capitalGainsTaxes.netCapitalGains,
+      };
+    });
+  }
+
+  extractSingleSimulationReturnsChartData(simulation: SimulationResult): SingleSimulationReturnsChartDataPoint[] {
+    const startAge = simulation.context.startAge;
+    const startDateYear = new Date().getFullYear();
+
+    return simulation.data.slice(1).map((data) => {
+      const currDateYear = new Date(data.date).getFullYear();
+
+      const returnsData = data.returns!;
+
+      return {
+        age: currDateYear - startDateYear + startAge,
+        stocksRate: returnsData.annualReturnRates.stocks,
+        bondsRate: returnsData.annualReturnRates.bonds,
+        cashRate: returnsData.annualReturnRates.cash,
+        inflationRate: returnsData.annualInflationRate,
+        totalStocksAmount: returnsData.totalReturnAmounts.stocks,
+        totalBondsAmount: returnsData.totalReturnAmounts.bonds,
+        totalCashAmount: returnsData.totalReturnAmounts.cash,
+        stocksAmount: returnsData.returnAmountsForPeriod.stocks,
+        bondsAmount: returnsData.returnAmountsForPeriod.bonds,
+        cashAmount: returnsData.returnAmountsForPeriod.cash,
+      };
+    });
+  }
+
+  extractSingleSimulationContributionsChartData(simulation: SimulationResult): SingleSimulationContributionsChartDataPoint[] {
+    const startAge = simulation.context.startAge;
+    const startDateYear = new Date().getFullYear();
+
+    return simulation.data.slice(1).map((data) => {
+      const currDateYear = new Date(data.date).getFullYear();
+
+      const portfolioData = data.portfolio;
+
+      let cashSavings = 0;
+      let taxableBrokerage = 0;
+      let taxDeferred = 0;
+      let taxFree = 0;
+
+      for (const account of Object.values(portfolioData.perAccountData)) {
+        switch (account.type) {
+          case 'savings':
+            cashSavings += account.contributionsForPeriod;
+            break;
+          case 'taxableBrokerage':
+            taxableBrokerage += account.contributionsForPeriod;
+            break;
+          case '401k':
+          case 'ira':
+          case 'hsa':
+            taxDeferred += account.contributionsForPeriod;
+            break;
+          case 'roth401k':
+          case 'rothIra':
+            taxFree += account.contributionsForPeriod;
+            break;
+        }
+      }
+
+      return {
+        age: currDateYear - startDateYear + startAge,
+        totalContributions: portfolioData.totalContributions,
+        annualContributions: portfolioData.contributionsForPeriod,
+        perAccountData: Object.values(portfolioData.perAccountData),
+        taxableBrokerage,
+        taxDeferred,
+        taxFree,
+        cashSavings,
+      };
+    });
+  }
+
+  extractSingleSimulationWithdrawalsChartData(simulation: SimulationResult): SingleSimulationWithdrawalsChartDataPoint[] {
+    const startAge = simulation.context.startAge;
+    const startDateYear = new Date().getFullYear();
+
+    return simulation.data.slice(1).map((data) => {
+      const currDateYear = new Date(data.date).getFullYear();
+
+      const portfolioData = data.portfolio;
+      const totalValue = portfolioData.totalValue;
+      const annualWithdrawals = portfolioData.withdrawalsForPeriod;
+
+      let cashSavings = 0;
+      let taxableBrokerage = 0;
+      let taxDeferred = 0;
+      let taxFree = 0;
+
+      for (const account of Object.values(portfolioData.perAccountData)) {
+        switch (account.type) {
+          case 'savings':
+            cashSavings += account.withdrawalsForPeriod;
+            break;
+          case 'taxableBrokerage':
+            taxableBrokerage += account.withdrawalsForPeriod;
+            break;
+          case '401k':
+          case 'ira':
+          case 'hsa':
+            taxDeferred += account.withdrawalsForPeriod;
+            break;
+          case 'roth401k':
+          case 'rothIra':
+            taxFree += account.withdrawalsForPeriod;
+            break;
+        }
+      }
+
+      const withdrawalRate = totalValue + annualWithdrawals > 0 ? (annualWithdrawals / (totalValue + annualWithdrawals)) * 100 : null;
+
+      return {
+        age: currDateYear - startDateYear + startAge,
+        totalWithdrawals: portfolioData.totalWithdrawals,
+        annualWithdrawals: portfolioData.withdrawalsForPeriod,
+        totalRealizedGains: portfolioData.totalRealizedGains,
+        annualRealizedGains: portfolioData.realizedGainsForPeriod,
+        perAccountData: Object.values(portfolioData.perAccountData),
+        taxableBrokerage,
+        taxDeferred,
+        taxFree,
+        cashSavings,
+        withdrawalRate,
+      };
+    });
+  }
+}
