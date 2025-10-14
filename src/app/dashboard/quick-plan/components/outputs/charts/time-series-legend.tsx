8a43e72183055cc59ddc1289ebad68e7143228b4
schelskedevco <joe@schelske.dev>
2025-10-14 15:12:07 -0500

    time series legend

--- src/app/dashboard/quick-plan/components/outputs/charts/time-series-legend.tsx
+++ src/app/dashboard/quick-plan/components/outputs/charts/time-series-legend.tsx
@@ -12,7 +12,7 @@ export default function TimeSeriesLegend({ colors, legendStrokeColor, dataKeys, 
 
   return (
     <div
-      className={cn('mt-2 gap-2 sm:gap-x-4', {
+      className={cn('divide-border/50 mt-2 gap-2 divide-x sm:gap-x-4', {
         'ml-16': !isSmallScreen,
         'grid grid-cols-2': useGridLayout,
         'sm:flex sm:justify-center': !useGridLayout,
@@ -21,7 +21,7 @@ export default function TimeSeriesLegend({ colors, legendStrokeColor, dataKeys, 
       aria-label="Chart legend"
     >
       {dataKeys.map((dataKey, index) => (
-        <div key={dataKey} className="flex items-center gap-x-2 text-sm font-medium">
+        <div key={dataKey} className="flex items-center gap-x-2 pr-4 text-sm font-medium">
           <svg viewBox="0 0 6 6" aria-hidden="true" style={{ fill: colors[index % colors.length] }} className="size-5 shrink-0">
             <rect x={0.5} y={0.5} width={5} height={5} stroke={legendStrokeColor} strokeWidth={0.5} paintOrder="stroke" />
           </svg>
