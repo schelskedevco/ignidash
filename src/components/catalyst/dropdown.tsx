4d98f1b16febbd625b35a31bccfa34c2bc94d115
schelskedevco <joe@schelske.dev>
2025-10-19 18:24:06 -0500

    dropdown border

--- src/components/catalyst/dropdown.tsx
+++ src/components/catalyst/dropdown.tsx
@@ -118,10 +118,7 @@ export function DropdownDivider({ className, ...props }: { className?: string } 
   return (
     <Headless.MenuSeparator
       {...props}
-      className={cn(
-        className,
-        'col-span-full mx-3.5 my-1 h-px border-0 bg-stone-950/5 sm:mx-3 dark:bg-white/10 forced-colors:bg-[CanvasText]'
-      )}
+      className={cn(className, 'bg-border/25 col-span-full mx-3.5 my-1 h-px border-0 sm:mx-3 forced-colors:bg-[CanvasText]')}
     />
   );
 }
