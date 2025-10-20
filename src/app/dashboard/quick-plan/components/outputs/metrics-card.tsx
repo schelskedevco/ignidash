8c1db949110a74622109e62ff1b6ac6d6cfea0b2
schelskedevco <joe@schelske.dev>
2025-10-19 22:19:13 -0500

    metrics card

--- src/app/dashboard/quick-plan/components/outputs/metrics-card.tsx
+++ src/app/dashboard/quick-plan/components/outputs/metrics-card.tsx
@@ -12,12 +12,11 @@ export default function MetricsCard({ name, stat, statContext, className }: Metr
   return (
     <Card className={cn('my-0 text-center sm:text-left', className)}>
       <dt className="text-muted-foreground truncate text-sm font-medium">{name}</dt>
-      <dd className="text-foreground mt-1 text-3xl font-semibold tracking-tight">
-        <span>{stat}</span>
-        <span className="text-muted-foreground ml-1 text-sm">{statContext}</span>
-        {/* <svg viewBox="0 0 18 18" aria-hidden="true" className="inline size-4.5 fill-green-500 dark:fill-green-400">
-          <circle r={9} cx={9} cy={9} />
-        </svg> */}
+      <dd className="text-foreground mt-1 sm:flex sm:items-center sm:justify-between">
+        <div className="text-3xl font-semibold tracking-tight">
+          <span>{stat}</span>
+          <span className="text-muted-foreground ml-1 text-sm">{statContext}</span>
+        </div>
       </dd>
     </Card>
   );
