96cf30eba45e87faa8d00bb4b78872e521fbbaf1
schelskedevco <joe@schelske.dev>
2025-10-23 18:33:43 -0500

    prettier ignore convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,7 +8,11 @@
  * @module
  */
 
-import type { ApiFromModules, FilterApi, FunctionReference } from 'convex/server';
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -21,7 +25,13 @@ import type { ApiFromModules, FilterApi, FunctionReference } from 'convex/server
 declare const fullApi: ApiFromModules<{}>;
 declare const fullApiWithMounts: typeof fullApi;
 
-export declare const api: FilterApi<typeof fullApiWithMounts, FunctionReference<any, 'public'>>;
-export declare const internal: FilterApi<typeof fullApiWithMounts, FunctionReference<any, 'internal'>>;
+export declare const api: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "public">
+>;
+export declare const internal: FilterApi<
+  typeof fullApiWithMounts,
+  FunctionReference<any, "internal">
+>;
 
 export declare const components: {};
