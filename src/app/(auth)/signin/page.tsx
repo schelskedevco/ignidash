c899fd105f5f47f7fcff1514bb2b85a2184cbd31
schelskedevco <joe@schelske.dev>
2025-10-24 00:14:37 -0500

    update content

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -15,7 +15,7 @@ export default function SignInPage() {
       <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <FireIcon className="text-primary mx-auto h-10 w-auto" />
-          <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+          <h2 className="mt-6 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Sign in to Ignidash</h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
