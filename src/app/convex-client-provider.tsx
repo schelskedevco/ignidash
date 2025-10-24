2bfb54a102ed1a5267884e4e4acd142c923d4d01
schelskedevco <joe@schelske.dev>
2025-10-23 23:17:58 -0500

    initial better auth setup

--- src/app/convex-client-provider.tsx
+++ src/app/convex-client-provider.tsx
@@ -1,10 +1,20 @@
 'use client';
 
-import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import { ConvexReactClient } from 'convex/react';
 import { ReactNode } from 'react';
+import { ConvexBetterAuthProvider } from '@convex-dev/better-auth/react';
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+import { authClient } from '@/lib/auth-client';
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
+  // Optionally pause queries until the user is authenticated
+  expectAuth: true,
+});
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ConvexBetterAuthProvider client={convex} authClient={authClient}>
+      {children}
+    </ConvexBetterAuthProvider>
+  );
 }
