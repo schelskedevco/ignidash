0d3cf10dfc07cbb6701654ee57dc34c5022e71e7
schelskedevco <joe@schelske.dev>
2025-10-26 15:46:05 -0500

    notification z inddex

--- src/components/ui/success-notification.tsx
+++ src/components/ui/success-notification.tsx
@@ -13,7 +13,7 @@ interface SuccessNotificationProps {
 
 export default function SuccessNotification({ title, desc, setShow, show }: SuccessNotificationProps) {
   return (
-    <div aria-live="assertive" className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6">
+    <div aria-live="assertive" className="pointer-events-none fixed inset-0 z-50 flex items-end px-4 py-6 sm:items-start sm:p-6">
       <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
         <Transition show={show}>
           <div className="bg-emphasized-background pointer-events-auto w-full max-w-sm rounded-lg shadow-lg outline-1 outline-black/5 transition data-closed:opacity-0 data-enter:transform data-enter:duration-300 data-enter:ease-out data-closed:data-enter:translate-y-2 data-leave:duration-100 data-leave:ease-in data-closed:data-enter:sm:translate-x-2 data-closed:data-enter:sm:translate-y-0 dark:-outline-offset-1 dark:outline-white/10">
