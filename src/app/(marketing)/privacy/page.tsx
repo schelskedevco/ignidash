a7ca0f7fccb48539777c486c31560bd6304c2cd1
schelskedevco <joe@schelske.dev>
2025-10-29 20:30:23 -0500

    privacy and terms

---
+++ src/app/(marketing)/privacy/page.tsx
@@ -0,0 +1,9 @@
+import MainArea from '@/components/layout/main-area';
+
+export default function PrivacyPage() {
+  return (
+    <MainArea hasSecondaryColumn={false}>
+      <h1 className="text-4xl font-extrabold tracking-tight">Privacy Policy</h1>
+    </MainArea>
+  );
+}
---
+++ src/app/(marketing)/terms/page.tsx
@@ -0,0 +1,9 @@
+import MainArea from '@/components/layout/main-area';
+
+export default function TermsPage() {
+  return (
+    <MainArea hasSecondaryColumn={false}>
+      <h1 className="text-4xl font-extrabold tracking-tight">Terms of Service</h1>
+    </MainArea>
+  );
+}
