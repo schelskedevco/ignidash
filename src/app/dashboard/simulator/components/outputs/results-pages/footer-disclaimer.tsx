bfdcb7d03acdea7fa1830eb012bf4480abc3b0f4
schelskedevco <joe@schelske.dev>
2025-11-03 12:00:24 -0600

    footer disclaimer

--- src/app/dashboard/simulator/components/outputs/results-pages/footer-disclaimer.tsx
+++ src/app/dashboard/simulator/components/outputs/results-pages/footer-disclaimer.tsx
@@ -3,9 +3,8 @@ import SectionContainer from '@/components/ui/section-container';
 export default function FooterDisclaimer() {
   return (
     <SectionContainer showBottomBorder={false} className="mb-0 py-4">
-      <p className="text-muted-foreground text-center text-xs/6">
-        For educational purposes only. Past performance is not indicative of future results.
-      </p>
+      <p className="text-muted-foreground text-center text-xs/6">For educational purposes only.</p>
+      <p className="text-muted-foreground text-center text-xs/6">Past performance not indicative of future results.</p>
     </SectionContainer>
   );
 }
