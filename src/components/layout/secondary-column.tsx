5c4b1709a47fb84e60d727d6afb421b5ae9e2b9d
schelskedevco <joe@schelske.dev>
2025-11-10 13:32:44 -0600

    small change

--- src/components/layout/secondary-column.tsx
+++ src/components/layout/secondary-column.tsx
@@ -1,8 +1,4 @@
-interface SecondaryColumnProps {
-  children?: React.ReactNode;
-}
-
-export default function SecondaryColumn({ children }: SecondaryColumnProps) {
+export default function SecondaryColumn({ children }: { children?: React.ReactNode }) {
   return (
     <aside className="border-border/50 fixed inset-y-0 left-72 hidden w-96 overflow-y-auto border-r px-2 group-data-[state=collapsed]/sidebar:left-16 sm:px-3 lg:px-4 xl:block">
       {children}
