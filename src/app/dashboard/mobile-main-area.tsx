12bf132f3966f242eecd83f6571e73bef6f12cd0
schelskedevco <joe@schelske.dev>
2025-11-10 14:09:02 -0600

    base layout

---
+++ src/app/dashboard/mobile-main-area.tsx
@@ -0,0 +1,7 @@
+export default function MobileMainArea() {
+  return (
+    <div className="flex h-full flex-col pt-[7.375rem] lg:hidden lg:pt-[4.3125rem]">
+      <h1>Mobile Main Area</h1>
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,26 +1,15 @@
 'use client';
 
-import { useMutation } from 'convex/react';
-import { api } from '@/convex/_generated/api';
-
 import MainArea from '@/components/layout/main-area';
-import { Button } from '@/components/catalyst/button';
+
+import DesktopMainArea from './desktop-main-area';
+import MobileMainArea from './mobile-main-area';
 
 export default function DashboardPage() {
-  const m = useMutation(api.plans.createBlankPlan);
-
   return (
     <MainArea hasSecondaryColumn={false}>
-      <h1 className="text-4xl font-extrabold tracking-tight">Dashboard</h1>
-      <Button
-        color="rose"
-        onClick={async () => {
-          const res = await m({ newPlanName: 'Demo Plan' });
-          console.log('Created new plan:', res);
-        }}
-      >
-        Create Simulation
-      </Button>
+      <MobileMainArea />
+      <DesktopMainArea />
     </MainArea>
   );
 }
