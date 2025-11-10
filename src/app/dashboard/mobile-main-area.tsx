4bcd4479d36c2269d2df4152a18f5e10ddfa9de7
schelskedevco <joe@schelske.dev>
2025-11-10 14:23:24 -0600

    4.0625

--- src/app/dashboard/mobile-main-area.tsx
+++ src/app/dashboard/mobile-main-area.tsx
@@ -1,6 +1,6 @@
 export default function MobileMainArea() {
   return (
-    <div className="flex h-full flex-col pt-[7.375rem] lg:hidden lg:pt-[4.3125rem]">
+    <div className="flex h-full flex-col pt-[4.0625rem] lg:hidden lg:pt-[4.3125rem]">
       <h1>Mobile Main Area</h1>
     </div>
   );
