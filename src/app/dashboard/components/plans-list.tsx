90248eff56f54626fd9036fecbc4c36727094c90
schelskedevco <joe@schelske.dev>
2025-11-11 02:14:45 -0600

    plans UI

--- src/app/dashboard/components/plans-list.tsx
+++ src/app/dashboard/components/plans-list.tsx
@@ -4,6 +4,9 @@ import { Preloaded, usePreloadedQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 
 import Card from '@/components/ui/card';
+import { Heading, Subheading } from '@/components/catalyst/heading';
+import { DescriptionDetails, DescriptionList, DescriptionTerm } from '@/components/catalyst/description-list';
+import { formatNumber } from '@/lib/utils';
 
 interface PlansListProps {
   preloadedPlans: Preloaded<typeof api.plans.listPlans>;
@@ -13,14 +16,29 @@ export default function PlansList({ preloadedPlans }: PlansListProps) {
   const plans = usePreloadedQuery(preloadedPlans);
 
   return (
-    <div className="mx-auto w-full max-w-3xl lg:max-w-5xl xl:max-w-7xl">
-      <div className="flex w-full flex-col gap-2">
+    <>
+      <Heading level={3} className="mt-4 mb-2">
+        Simulations
+      </Heading>
+      <div className="grid w-full grid-cols-1 gap-2 lg:grid-cols-2">
         {plans.map((plan) => (
           <Card key={plan._id} className="w-full">
-            Plan Name: {plan.name}
+            <Subheading>{plan.name}</Subheading>
+            <DescriptionList>
+              <DescriptionTerm>Portfolio Value</DescriptionTerm>
+              <DescriptionDetails>
+                {formatNumber(
+                  plan.accounts.reduce((total, account) => total + account.balance, 0),
+                  0,
+                  '$'
+                )}
+              </DescriptionDetails>
+              <DescriptionTerm>Retirement Strategy</DescriptionTerm>
+              <DescriptionDetails>...</DescriptionDetails>
+            </DescriptionList>
           </Card>
         ))}
       </div>
-    </div>
+    </>
   );
 }
