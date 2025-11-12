86c9f1026a4e3aacad6373858be8227919e2b7a3
schelskedevco <joe@schelske.dev>
2025-11-12 15:46:13 -0600

    use name

--- src/app/dashboard/components/dashboard-column-header.tsx
+++ src/app/dashboard/components/dashboard-column-header.tsx
@@ -1,11 +1,23 @@
+'use client';
+
 import { LayoutDashboardIcon } from 'lucide-react';
+import { Preloaded, usePreloadedQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 import ColumnHeader from '@/components/ui/column-header';
 
-export default function DashboardColumnHeader() {
+interface DashboardColumnHeaderProps {
+  preloadedUser: Preloaded<typeof api.auth.getCurrentUserSafe>;
+}
+
+export default function DashboardColumnHeader({ preloadedUser }: DashboardColumnHeaderProps) {
+  const user = usePreloadedQuery(preloadedUser);
+
+  const name = user?.name ?? 'Anonymous';
+
   return (
     <ColumnHeader
-      title="Dashboard"
+      title={`Welcome back, ${name.split(' ')[0]}!`}
       icon={LayoutDashboardIcon}
       className="h-[4.3125rem] w-[calc(100%-18rem)] group-data-[state=collapsed]/sidebar:w-[calc(100%-4rem)]"
     />
--- src/app/dashboard/components/desktop-main-area.tsx
+++ src/app/dashboard/components/desktop-main-area.tsx
@@ -1,10 +1,20 @@
+import { preloadQuery } from 'convex/nextjs';
+import { api } from '@/convex/_generated/api';
+
+import { getToken } from '@/lib/auth-server';
+
 import DashboardColumnHeader from './dashboard-column-header';
 import DashboardContent from './dashboard-content';
 
-export default function DesktopMainArea() {
+export default async function DesktopMainArea() {
+  const token = await getToken();
+  if (!token) throw new Error('User not authenticated');
+
+  const preloadedUser = await preloadQuery(api.auth.getCurrentUserSafe, {}, { token });
+
   return (
     <div className="hidden lg:block">
-      <DashboardColumnHeader />
+      <DashboardColumnHeader preloadedUser={preloadedUser} />
       <div className="flex h-full flex-col pt-[4.3125rem]">
         <DashboardContent />
       </div>
