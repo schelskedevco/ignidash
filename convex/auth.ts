2951629d1d569bf79fb2a69b3d78d677cea0a551
schelskedevco <joe@schelske.dev>
2025-11-25 20:37:50 -0600

    update betterAuth session stuff

--- convex/auth.ts
+++ convex/auth.ts
@@ -26,6 +26,15 @@ const rateLimiter = new RateLimiter(components.rateLimiter, {
 
 export const createAuth = (ctx: GenericCtx<DataModel>, { optionsOnly } = { optionsOnly: false }) => {
   return betterAuth({
+    session: {
+      expiresIn: 60 * 60 * 24 * 7, // 7 days
+      updateAge: 60 * 60 * 24, // 1 day (every 1 day the session expiration is updated)
+      cookieCache: {
+        enabled: true,
+        maxAge: 5 * 60,
+        strategy: 'jwt',
+      },
+    },
     logger: {
       disabled: optionsOnly,
     },
