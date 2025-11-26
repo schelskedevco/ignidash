fac963bb65b1d79ce1fb0956ec0c74302a2e2865
schelskedevco <joe@schelske.dev>
2025-11-25 19:58:28 -0600

    don't autofocus sim settings

--- src/app/dashboard/simulator/[planId]/components/outputs/drawers/simulation-settings-drawer.tsx
+++ src/app/dashboard/simulator/[planId]/components/outputs/drawers/simulation-settings-drawer.tsx
@@ -137,7 +137,6 @@ export default function SimulationSettingsDrawer({ setOpen, simulationSettings }
                       min={1}
                       max={9999}
                       disableThousandsSeparator
-                      autoFocus
                     />
                     {errors.simulationSeed && <ErrorMessage>{errors.simulationSeed?.message}</ErrorMessage>}
                     <Description>
