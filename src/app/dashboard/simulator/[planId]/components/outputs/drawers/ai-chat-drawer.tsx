d63a25f447b4454a3696a277b4487eb2e53bd9fc
schelskedevco <joe@schelske.dev>
2025-12-01 14:57:27 -0600

    small changes

--- src/app/dashboard/simulator/[planId]/components/outputs/drawers/ai-chat-drawer.tsx
+++ src/app/dashboard/simulator/[planId]/components/outputs/drawers/ai-chat-drawer.tsx
@@ -4,8 +4,10 @@ interface AIChatDrawerProps {
 
 export default function AIChatDrawer({ setOpen }: AIChatDrawerProps) {
   return (
-    <div className="-mx-2 hidden sm:-mx-3 md:fixed md:inset-y-[4.8125rem] md:flex md:w-64 md:flex-col">
-      <div className="border-border/50 flex grow flex-col border-r bg-zinc-50 dark:bg-black/10">Sidebar</div>
-    </div>
+    <>
+      <div className="hidden md:fixed md:top-[4.8125rem] md:bottom-0 md:-mx-3 md:flex md:w-64 md:flex-col">
+        <div className="border-border/50 flex grow flex-col border-r bg-zinc-50 dark:bg-black/10">Sidebar</div>
+      </div>
+    </>
   );
 }
