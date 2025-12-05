88ca56c4e1834442425302ce8cde1ac2eae4934a
schelskedevco <joe@schelske.dev>
2025-12-04 22:39:03 -0600

    update pricing page

--- src/app/(marketing)/pricing/page.tsx
+++ src/app/(marketing)/pricing/page.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import { CheckIcon } from '@heroicons/react/20/solid';
+import Link from 'next/link';
 
 import { cn } from '@/lib/utils';
+import { authClient } from '@/lib/auth-client';
 
 const tiers = [
   {
@@ -93,18 +97,33 @@ export default function PricingPage() {
                 </li>
               ))}
             </ul>
-            <button
-              disabled
-              aria-describedby={tier.id}
-              className={cn(
-                tier.featured
-                  ? 'bg-rose-500 text-white shadow-xs hover:bg-rose-400 focus-visible:outline-rose-500 dark:shadow-none'
-                  : 'text-rose-600 inset-ring inset-ring-rose-200 hover:inset-ring-rose-300 focus-visible:outline-rose-600 dark:bg-white/10 dark:text-white dark:inset-ring-white/5 dark:hover:bg-white/20 dark:hover:inset-ring-white/5 dark:focus-visible:outline-white/75',
-                'mt-8 block w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed sm:mt-10'
-              )}
-            >
-              Coming Soon
-            </button>
+            {tier.id === 'tier-pro' ? (
+              <button
+                aria-describedby={tier.id}
+                onClick={async () => await authClient.checkout({ slug: 'Ignidash-Pro' })}
+                className={cn(
+                  tier.featured
+                    ? 'bg-rose-500 text-white shadow-xs hover:bg-rose-400 focus-visible:outline-rose-500 dark:shadow-none'
+                    : 'text-rose-600 inset-ring inset-ring-rose-200 hover:inset-ring-rose-300 focus-visible:outline-rose-600 dark:bg-white/10 dark:text-white dark:inset-ring-white/5 dark:hover:bg-white/20 dark:hover:inset-ring-white/5 dark:focus-visible:outline-white/75',
+                  'mt-8 block w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed sm:mt-10'
+                )}
+              >
+                Buy {tier.name}
+              </button>
+            ) : (
+              <Link
+                href={tier.href}
+                aria-describedby={tier.id}
+                className={cn(
+                  tier.featured
+                    ? 'bg-rose-500 text-white shadow-xs hover:bg-rose-400 focus-visible:outline-rose-500 dark:shadow-none'
+                    : 'text-rose-600 inset-ring inset-ring-rose-200 hover:inset-ring-rose-300 focus-visible:outline-rose-600 dark:bg-white/10 dark:text-white dark:inset-ring-white/5 dark:hover:bg-white/20 dark:hover:inset-ring-white/5 dark:focus-visible:outline-white/75',
+                  'mt-8 block rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 sm:mt-10'
+                )}
+              >
+                Start your plan
+              </Link>
+            )}
           </div>
         ))}
       </div>
