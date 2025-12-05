83678ec66ac65eb590f06752a1d4608412935571
schelskedevco <joe@schelske.dev>
2025-12-05 15:30:35 -0600

    updates

--- convex/convex.config.ts
+++ convex/convex.config.ts
@@ -2,10 +2,12 @@ import { defineApp } from 'convex/server';
 import betterAuth from './betterAuth/convex.config';
 import resend from '@convex-dev/resend/convex.config';
 import rateLimiter from '@convex-dev/rate-limiter/convex.config';
+import migrations from '@convex-dev/migrations/convex.config.js';
 
 const app = defineApp();
 app.use(resend);
 app.use(betterAuth);
 app.use(rateLimiter);
+app.use(migrations);
 
 export default app;
