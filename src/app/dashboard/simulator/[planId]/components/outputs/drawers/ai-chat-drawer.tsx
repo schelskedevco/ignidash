3858a1602d5ac3f628710565360ab659fcb05cb2
Joe Schelske <joe@schelske.dev>
2025-12-09 16:14:19 -0600

    mark as aside

--- src/app/dashboard/simulator/[planId]/components/outputs/drawers/ai-chat-drawer.tsx
+++ src/app/dashboard/simulator/[planId]/components/outputs/drawers/ai-chat-drawer.tsx
@@ -17,13 +17,13 @@ export default function AIChatDrawer({ setOpen }: AIChatDrawerProps) {
 
   return (
     <>
-      <div className="hidden md:fixed md:top-[4.8125rem] md:bottom-0 md:-mx-3 md:flex md:w-64 md:flex-col">
+      <aside className="hidden md:fixed md:top-[4.8125rem] md:bottom-0 md:-mx-3 md:flex md:w-64 md:flex-col">
         <div className="border-border/50 flex grow flex-col border-r bg-zinc-50 dark:bg-black/10">
           <Button onClick={async () => await m({ conversationId: undefined, planId, content: 'What is a taxable brokerage account?' })}>
             Send Demo Message
           </Button>
         </div>
-      </div>
+      </aside>
     </>
   );
 }
