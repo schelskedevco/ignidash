bef64d4e78ee786dc6d3abac06c98a2aeb2997b3
Joe Schelske <joe@schelske.dev>
2025-12-09 15:55:46 -0600

    ai chat drawer

--- src/app/dashboard/simulator/[planId]/components/outputs/drawers/ai-chat-drawer.tsx
+++ src/app/dashboard/simulator/[planId]/components/outputs/drawers/ai-chat-drawer.tsx
@@ -1,22 +1,25 @@
 'use client';
 
 import { api } from '@/convex/_generated/api';
-import { useAction } from 'convex/react';
+import { useMutation } from 'convex/react';
 
 import { Button } from '@/components/catalyst/button';
+import { useSelectedPlanId } from '@/hooks/use-selected-plan-id';
 
 interface AIChatDrawerProps {
   setOpen: (open: boolean) => void;
 }
 
 export default function AIChatDrawer({ setOpen }: AIChatDrawerProps) {
-  const a = useAction(api.use_openai.chat);
+  const planId = useSelectedPlanId();
+
+  const m = useMutation(api.messages.send);
 
   return (
     <>
       <div className="hidden md:fixed md:top-[4.8125rem] md:bottom-0 md:-mx-3 md:flex md:w-64 md:flex-col">
         <div className="border-border/50 flex grow flex-col border-r bg-zinc-50 dark:bg-black/10">
-          <Button onClick={async () => await a({ messages: [{ content: 'What is a taxable brokerage account?', role: 'user' }] })}>
+          <Button onClick={async () => await m({ conversationId: undefined, planId, content: 'Send demo message' })}>
             Send Demo Message
           </Button>
         </div>
