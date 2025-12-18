560699f5eec65c9dc1dd60fbd50390346c4af338
Joe Schelske <joe@schelske.dev>
2025-12-17 20:53:16 -0600

    dp validator

--- convex/validators/simulation_result_validator.ts
+++ convex/validators/simulation_result_validator.ts
@@ -1,7 +1,18 @@
 import { v, type Infer } from 'convex/values';
 
+export const simulationDataPointValidator = v.object({
+  age: v.number(),
+  stockHoldings: v.number(),
+  bondHoldings: v.number(),
+  cashHoldings: v.number(),
+  taxableValue: v.number(),
+  taxDeferredValue: v.number(),
+  taxFreeValue: v.number(),
+  cashSavings: v.number(),
+});
+
 export const simulationResultValidator = v.object({
-  simulationResult: v.array(v.string()),
+  simulationResult: v.array(simulationDataPointValidator),
 });
 
 export type SimulationResult = Infer<typeof simulationResultValidator>;
