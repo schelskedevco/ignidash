c01099ad0ae47396bd70ee3f5927aaeed3968b72
Joe Schelske <joe@schelske.dev>
2025-12-21 13:00:40 -0600

    disabled opacity

--- src/app/(marketing)/pricing/buy-pro-button.tsx
+++ src/app/(marketing)/pricing/buy-pro-button.tsx
@@ -25,7 +25,7 @@ export default function BuyProButton({ tier, preloadedSubscriptions, isAuthentic
     tier.featured
       ? 'bg-rose-500 text-white shadow-xs hover:bg-rose-400 focus-visible:outline-rose-500 dark:shadow-none'
       : 'text-rose-600 inset-ring inset-ring-rose-200 hover:inset-ring-rose-300 focus-visible:outline-rose-600 dark:bg-white/10 dark:text-white dark:inset-ring-white/5 dark:hover:bg-white/20 dark:hover:inset-ring-white/5 dark:focus-visible:outline-white/75',
-    'mt-8 block w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed disabled:opacity-75 sm:mt-10'
+    'mt-8 block w-full rounded-md px-3.5 py-2.5 text-center text-sm font-semibold focus-visible:outline-2 focus-visible:outline-offset-2 disabled:cursor-not-allowed  sm:mt-10'
   );
 
   const pathname = usePathname();
