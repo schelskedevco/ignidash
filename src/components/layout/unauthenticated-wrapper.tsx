e62e9b6adbbcc4ccececf65cdbc56bcebb6e7716
Joe Schelske <joe@schelske.dev>
2026-02-28 16:58:17 -0600

    small change to pageloading

--- src/components/layout/unauthenticated-wrapper.tsx
+++ src/components/layout/unauthenticated-wrapper.tsx
@@ -6,6 +6,13 @@ import PageLoading from '@/components/ui/page-loading';
 
 export default function UnauthenticatedWrapper({ children }: { children: React.ReactNode }) {
   const { isAuthenticated, isLoading } = useConvexAuth();
-  if (!isAuthenticated && !isLoading) return <PageLoading message="Not authenticated, please refresh the page" />;
+  if (!isAuthenticated && !isLoading) {
+    return (
+      <div className="h-full group-data-[animating=true]/sidebar:transition-[padding-left] group-data-[animating=true]/sidebar:duration-200 group-data-[animating=true]/sidebar:ease-in-out motion-reduce:transition-none lg:pl-72 group-data-[state=collapsed]/sidebar:lg:pl-16">
+        <PageLoading message="Not authenticated, please refresh the page" />
+      </div>
+    );
+  }
+
   return <>{children}</>;
 }
